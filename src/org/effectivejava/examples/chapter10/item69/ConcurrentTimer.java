0859fdcdd5f7e92ba353aa11bb69605fc0d66774
jianghao <jianghao7625@gmail.com>
2019-01-11 22:08:42 +0800

    提交第69条目代码

--- src/org/effectivejava/examples/chapter10/item69/ConcurrentTimer.java
+++ src/org/effectivejava/examples/chapter10/item69/ConcurrentTimer.java
@@ -7,14 +7,21 @@ import java.util.concurrent.Executors;
 
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
-/**
- * 正常情况下引入ready和done两个CountDownLatch就可以计算时间, 之所以引入start可以更加精确的
- * 计算程序执行时间, ready和done两个初始化值为concurrency并发数, 它们在countDown为零之后会
- * 依次唤醒线程, 这个线程唤醒过程存在延迟, 而引入一个初始值为1的start变量则可以消除这一点
- */
 public class ConcurrentTimer {
-    private ConcurrentTimer() {}
+    private ConcurrentTimer() {
+    }
 
+    /**
+     * 1. 正常情况下引入ready和done两个CountDownLatch就可以计算时间, 之所以引入start可以更加精
+     * 确的计算程序执行时间, ready和done两个初始化值为concurrency并发数, 它们在countDown为零之
+     * 后会依次唤醒线程, 这个线程唤醒过程存在延迟, 而引入一个初始值为1的start变量则可以消除这一点
+     * <p>
+     * 2. 运用此方法需要注意time传递的参数是一个Runnable, 也就是说多线程共享一个Runnable, 在run
+     * 方法内尽量不要涉及实例变量, 否则会发生数据竞争, 如果避免不了, 那么请使用同步或者修改time方法
+     * 为多个Runnable实例
+     * <p>
+     * 3. 其他注意事项请参见effective java第69条目
+     */
     private static long time(ExecutorService executor, int concurrency,
                              final Runnable action) throws InterruptedException {
         final CountDownLatch ready = new CountDownLatch(concurrency);
